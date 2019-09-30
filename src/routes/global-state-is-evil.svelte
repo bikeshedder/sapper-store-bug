113b75112367d3c83d971dce729a02d43278cde1
Michael P. Jung <michael.jung@terreon.de>
2019-09-30 13:09:25 +0200

    Add global state is evil example

---
+++ src/routes/global-state-is-evil.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+    import { name as name3 } from './_stores'
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+	export async function preload({ params, query }) {
+        // this is only possible because the store is not reset
+        // on a per-request basis.
+        for (var i=0; i<60; i++) {
+            name3.set(`evil-${i}`)
+            await sleep(1000);
+        }
+	}
+</script>
+
+<h1>This is evil.</h1>
