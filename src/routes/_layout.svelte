a13681d0e32e50e0407827079ba7c9954b23d9d2
Michael P. Jung <michael.jung@terreon.de>
2019-09-30 02:00:28 +0200

    Initial commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { setContext } from 'svelte'
+	import { writable } from 'svelte/store'
+	import { stores } from '@sapper/app'
+
+	export const foo = writable()
+	stores().foo = foo
+
+	export const bar = writable()
+	setContext('bar', bar)
+</script>
+
+<main>
+	<h2>_layout.svelte :: foo = {$foo}</h2>
+	<h2>_layout.svelet :: bar = {$bar}</h2>
+	<slot></slot>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+	export function preload({ params, query }) {
+		return { name: 'borat' }
+	}
+</script>
+
+<script>
+	export let name;
+
+	import { stores } from '@sapper/app'
+	import { getContext } from 'svelte'
+
+	const foo = stores().foo
+	foo.set(name)
+
+	const bar = getContext('bar')
+	bar.set(name)
+</script>
+
+<h2>routes/index.svelte :: $foo = {$foo}</h2>
+<h2>routes/index.svelte :: $bar = {$bar}</h2>
